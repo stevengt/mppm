c1e8403b836108b792b2588566d38dba7976de8f
Steven Thomas <stevent3115@gmail.com>
2019-11-20 21:56:07 -0500

    Updated Ableton component types.

--- packaging/package/components/daws/ableton/types.go
+++ packaging/package/components/daws/ableton/types.go
@@ -5,7 +5,8 @@ type AbletonComponentType int32
 const (
 	AnalysisFile     AbletonComponentType = iota
 	DevicePreset     AbletonComponentType = iota
-	DeviceRackPreset AbletonComponentType = iota
+	DeviceChain      AbletonComponentType = iota
+	DeviceRack       AbletonComponentType = iota
 	Groove           AbletonComponentType = iota
 	LiveClip         AbletonComponentType = iota
 	LivePack         AbletonComponentType = iota
