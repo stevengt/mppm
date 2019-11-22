bd64838b7b6d1933594d8d0a6d59dfd89c1f10cf
Steven Thomas <stevent3115@gmail.com>
2019-11-21 23:46:31 -0500

    Added component types.

--- packaging/package/components/types.go
+++ packaging/package/components/types.go
@@ -1,13 +1,33 @@
 package components
 
-type ComponentType int32
+type ComponentType string
 
 const (
-	DAWSpecific ComponentType = iota
-	Driver      ComponentType = iota
-	Patch       ComponentType = iota
-	Plugin      ComponentType = iota
-	Preset      ComponentType = iota
-	Sample      ComponentType = iota
-	Soundbank   ComponentType = iota
+	DAWSpecific ComponentType = "DAWSpecific"
+	Driver      ComponentType = "Driver"
+	Patch       ComponentType = "Patch"
+	Plugin      ComponentType = "Plugin"
+	Preset      ComponentType = "Preset"
+	Sample      ComponentType = "Sample"
+	Soundbank   ComponentType = "Soundbank"
 )
+
+func GetComponentTypeFromString(typeName string) (componentType ComponentType) {
+	switch typeName {
+	case "DAWSpecific":
+		componentType = DAWSpecific
+	case "Driver":
+		componentType = Driver
+	case "Patch":
+		componentType = Patch
+	case "Plugin":
+		componentType = Plugin
+	case "Preset":
+		componentType = Preset
+	case "Sample":
+		componentType = Sample
+	case "Soundbank":
+		componentType = Soundbank
+	}
+	return
+}
