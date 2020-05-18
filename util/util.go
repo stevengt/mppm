f6b31b271dd60b1ebde998c4df45c4799aa1c9ec
Steven Thomas <stevent3115@gmail.com>
2020-05-18 15:26:57 -0400

    Refactored util package.

--- util/util.go
+++ util/util.go
@@ -2,26 +2,42 @@ package util
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 )
 
 func ExecuteShellCommand(commandName string, args ...string) (err error) {
-	cmd := exec.Command(commandName, args...)
-	stdout, err := cmd.Output()
-
-	if stdout != nil {
-		fmt.Println(string(stdout))
+	stdout, err := ExecuteShellCommandAndReturnOutput(commandName, args...)
+	if len(stdout) > 0 {
+		fmt.Println(stdout)
 	}
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
 	return
 }
 
 func ExecuteShellCommandAndReturnOutput(commandName string, args ...string) (stdout string, err error) {
-	cmd := exec.Command(commandName, args...)
-	stdoutAsByteArray, err := cmd.Output()
-	stdout = string(stdoutAsByteArray)
+	cmdArgsArray := append([]string{commandName}, args...)
+	stdoutAsByteArray, err := exec.Command("/usr/bin/env", cmdArgsArray...).CombinedOutput()
+	if stdoutAsByteArray != nil && len(stdoutAsByteArray) > 0 {
+		stdout = string(stdoutAsByteArray)
+	}
+	return
+}
+
+func CopyFile(sourceFileName string, targetFileName string) (err error) {
+
+	source, err := os.Open(sourceFileName)
+	if err != nil {
+		return
+	}
+	defer source.Close()
+
+	destination, err := os.Create(targetFileName)
+	if err != nil {
+		return
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
 	return
 }
