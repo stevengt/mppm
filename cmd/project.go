e005dfc81450918731e9bf42428859e3cfa7b9f0
Steven Thomas <stevent3115@gmail.com>
2020-05-24 17:51:03 -0400

    Added 'commit-all' flag to project command.

--- cmd/project.go
+++ cmd/project.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/stevengt/mppm/config"
+	"github.com/stevengt/mppm/util"
 )
 
 func init() {
@@ -10,6 +11,7 @@ func init() {
 	cobra.OnInitialize(
 		func() {
 			isPreviewCommand, _ = projectCmd.PersistentFlags().GetBool("preview")
+			isCommitAllCommand, _ = projectCmd.Flags().GetBool("commit-all")
 		},
 	)
 
@@ -21,11 +23,20 @@ func init() {
 		"Shows what files will be affected without actually making changes.",
 	)
 
+	projectCmd.Flags().BoolVarP(
+		&isCommitAllCommand,
+		"commit-all",
+		"c",
+		false,
+		"Equivalent to running 'mppm project extract; git add . -A; git commit -m '<commit message>'.",
+	)
+
 	rootCmd.AddCommand(projectCmd)
 
 }
 
 var isPreviewCommand bool
+var isCommitAllCommand bool
 
 var projectCmd = &cobra.Command{
 
@@ -42,4 +53,31 @@ var projectCmd = &cobra.Command{
 			config.LoadMppmProjectConfig()
 		}
 	},
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if isCommitAllCommand {
+			commitAll(args[0])
+		} else {
+			cmd.Help()
+		}
+	},
+}
+
+func commitAll(commitMessage string) {
+
+	err := extractAllCompressedFiles()
+	if err != nil {
+		util.ExitWithError(err)
+	}
+
+	err = util.ExecuteShellCommand("git", "add", ".", "-A")
+	if err != nil {
+		util.ExitWithError(err)
+	}
+
+	err = util.ExecuteShellCommand("git", "commit", "-m", commitMessage)
+	if err != nil {
+		util.ExitWithError(err)
+	}
+
 }
