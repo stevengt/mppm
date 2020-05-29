425319fafc8702a8f4849ddd71056443e85aa484
Steven Thomas <stevent3115@gmail.com>
2020-05-28 21:29:21 -0400

    Implemented library_add command.

--- cmd/library_add.go
+++ cmd/library_add.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
+	"github.com/stevengt/mppm/config"
+	"github.com/stevengt/mppm/util"
 )
 
 func init() {
@@ -19,4 +23,60 @@ var libraryAddCmd = &cobra.Command{
 	Long: "Adds a library (folder) to track globally on your system.",
 
 	Args: cobra.MinimumNArgs(1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		err := addLibrary(args[0])
+		if err != nil {
+			util.ExitWithError(err)
+		}
+	},
+}
+
+func addLibrary(libraryFilePath string) (err error) {
+
+	err = util.ExecuteShellCommand("git", "-C", libraryFilePath, "init")
+	if err != nil {
+		return
+	}
+
+	err = util.ExecuteShellCommand("git", "-C", libraryFilePath, "lfs", "install")
+	if err != nil {
+		return
+	}
+
+	err = util.ExecuteShellCommand("git", "-C", libraryFilePath, "lfs", "track", "*")
+	if err != nil {
+		return
+	}
+
+	err = util.ExecuteShellCommand("git", "-C", libraryFilePath, "add", "-A", ".")
+	if err != nil {
+		return
+	}
+
+	err = util.ExecuteShellCommand("git", "-C", libraryFilePath, "commit", "-m", "Initial commit.")
+	if err != nil {
+		return
+	}
+
+	libraryGitCommitId, err := util.ExecuteShellCommandAndReturnOutput("git", "-C", libraryFilePath, "rev-parse", "HEAD")
+	if err != nil {
+		return
+	}
+	libraryGitCommitId = strings.Trim(libraryGitCommitId, " \n")
+
+	libraryConfig := &config.LibraryConfig{
+		FilePath:              libraryFilePath,
+		MostRecentGitCommitId: libraryGitCommitId,
+		CurrentGitCommitId:    libraryGitCommitId,
+	}
+
+	config.MppmGlobalConfig.Libraries = append(config.MppmGlobalConfig.Libraries, libraryConfig)
+	err = config.MppmGlobalConfig.SaveAsGlobalConfig()
+	if err != nil {
+		return
+	}
+
+	return
+
 }
