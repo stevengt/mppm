249818bf7c39ddb622155406fa3c37724c0e28d3
Steven Thomas <stevent3115@gmail.com>
2020-05-30 17:40:36 -0400

    Implemented util.Exiter interface.

--- util/exit.go
+++ util/exit.go
@@ -6,10 +6,45 @@ import (
 )
 
 func ExitWithError(err error) {
-	ExitWithErrorMessage(err.Error())
+	getExiter().ExitWithError(err)
 }
 
 func ExitWithErrorMessage(errorMessage string) {
+	getExiter().ExitWithErrorMessage(errorMessage)
+}
+
+var exiterFactory ExiterCreator = &CurrentProcessExiterCreator{}
+var exiter Exiter
+
+func getExiter() Exiter {
+	if exiter == nil {
+		exiter = exiterFactory.NewExiter()
+	}
+	return exiter
+}
+
+type ExiterCreator interface {
+	NewExiter() Exiter
+}
+
+type CurrentProcessExiterCreator struct{}
+
+func (currentProcessExiterCreator *CurrentProcessExiterCreator) NewExiter() Exiter {
+	return &CurrentProcessExiter{}
+}
+
+type Exiter interface {
+	ExitWithError(err error)
+	ExitWithErrorMessage(errorMessage string)
+}
+
+type CurrentProcessExiter struct{}
+
+func (currentProcessExiter *CurrentProcessExiter) ExitWithError(err error) {
+	currentProcessExiter.ExitWithErrorMessage(err.Error())
+}
+
+func (currentProcessExiter *CurrentProcessExiter) ExitWithErrorMessage(errorMessage string) {
 	fmt.Println("ERROR: " + errorMessage)
 	os.Exit(1)
 }
