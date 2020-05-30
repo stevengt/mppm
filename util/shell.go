6ced6cf5da39783a26735d24fb0712c569a6f2ca
Steven Thomas <stevent3115@gmail.com>
2020-05-30 17:32:19 -0400

    Implemented ShellCommandManager interface.

--- util/shell.go
+++ util/shell.go
@@ -6,14 +6,49 @@ import (
 )
 
 func ExecuteShellCommand(commandName string, args ...string) (err error) {
-	stdout, err := ExecuteShellCommandAndReturnOutput(commandName, args...)
+	return getShellCommandManager().ExecuteShellCommand(commandName, args...)
+}
+
+func ExecuteShellCommandAndReturnOutput(commandName string, args ...string) (stdout string, err error) {
+	return getShellCommandManager().ExecuteShellCommandAndReturnOutput(commandName, args...)
+}
+
+var shellCommandManagerFactory ShellCommandManagerCreator = &LocalShellCommandManagerCreator{}
+var shellCommandManager ShellCommandManager
+
+func getShellCommandManager() ShellCommandManager {
+	if shellCommandManager == nil {
+		shellCommandManager = shellCommandManagerFactory.NewShellCommandManager()
+	}
+	return shellCommandManager
+}
+
+type ShellCommandManagerCreator interface {
+	NewShellCommandManager() ShellCommandManager
+}
+
+type LocalShellCommandManagerCreator struct{}
+
+func (localShellCommandManagerCreator *LocalShellCommandManagerCreator) NewShellCommandManager() ShellCommandManager {
+	return &LocalShellCommandManager{}
+}
+
+type ShellCommandManager interface {
+	ExecuteShellCommand(commandName string, args ...string) (err error)
+	ExecuteShellCommandAndReturnOutput(commandName string, args ...string) (stdout string, err error)
+}
+
+type LocalShellCommandManager struct{}
+
+func (localShellCommandManager *LocalShellCommandManager) ExecuteShellCommand(commandName string, args ...string) (err error) {
+	stdout, err := localShellCommandManager.ExecuteShellCommandAndReturnOutput(commandName, args...)
 	if len(stdout) > 0 {
 		fmt.Println(stdout)
 	}
 	return
 }
 
-func ExecuteShellCommandAndReturnOutput(commandName string, args ...string) (stdout string, err error) {
+func (localShellCommandManager *LocalShellCommandManager) ExecuteShellCommandAndReturnOutput(commandName string, args ...string) (stdout string, err error) {
 	stdoutAsByteArray, err := exec.Command(commandName, args...).CombinedOutput()
 	if stdoutAsByteArray != nil && len(stdoutAsByteArray) > 0 {
 		stdout = string(stdoutAsByteArray)
