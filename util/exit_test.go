f9fba9d71d077dc39a69c5e37045d7ed4f6481f4
Steven Thomas <stevent3115@gmail.com>
2020-06-01 04:38:46 -0400

    Added WasExited field to MockExiter.

--- util/exit_test.go
+++ util/exit_test.go
@@ -1,11 +1,15 @@
 package util_test
 
-type MockExiter struct{}
+type MockExiter struct {
+	WasExited bool
+}
 
 func (mockExiter *MockExiter) ExitWithError(err error) {
+	mockExiter.WasExited = true
 	return
 }
 
 func (mockExiter *MockExiter) ExitWithErrorMessage(errorMessage string) {
+	mockExiter.WasExited = true
 	return
 }
