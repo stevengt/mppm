77fac8ddf231aeb1b4ac3a018b36bcdb5f914ca6
Steven Thomas <stevent3115@gmail.com>
2020-06-01 16:02:59 -0400

    Implemented MockGitManager.

--- util/git_test.go
+++ util/git_test.go
@@ -298,3 +298,49 @@ func TestAddAllAndCommit(t *testing.T) {
 	assert.Equal(t, err.Error(), "There was a problem committing items to the git repository.")
 
 }
+
+// ------------------------------------------------------------------------------
+
+type MockGitManager struct {
+	InitError            error
+	AddError             error
+	CommitError          error
+	CheckoutError        error
+	RevParseStdout       string
+	RevParseError        error
+	LfsInstallError      error
+	LfsTrackError        error
+	AddAllAndCommitError error
+}
+
+func (mockGitManager *MockGitManager) Init() (err error) {
+	return mockGitManager.InitError
+}
+
+func (mockGitManager *MockGitManager) Add(args ...string) (err error) {
+	return mockGitManager.AddError
+}
+
+func (mockGitManager *MockGitManager) Commit(args ...string) (err error) {
+	return mockGitManager.CommitError
+}
+
+func (mockGitManager *MockGitManager) Checkout(args ...string) (err error) {
+	return mockGitManager.CheckoutError
+}
+
+func (mockGitManager *MockGitManager) RevParse(args ...string) (stdout string, err error) {
+	return mockGitManager.RevParseStdout, mockGitManager.RevParseError
+}
+
+func (mockGitManager *MockGitManager) LfsInstall() (err error) {
+	return mockGitManager.LfsInstallError
+}
+
+func (mockGitManager *MockGitManager) LfsTrack(args ...string) (err error) {
+	return mockGitManager.LfsTrackError
+}
+
+func (mockGitManager *MockGitManager) AddAllAndCommit(commitMessage string) (err error) {
+	return mockGitManager.AddAllAndCommitError
+}
