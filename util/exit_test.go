3485bf856ba76016392301fadc4dff4ba24494fa
Steven Thomas <stevent3115@gmail.com>
2020-06-01 15:53:15 -0400

    Added ErrorMessage field to MockExiter.

--- util/exit_test.go
+++ util/exit_test.go
@@ -1,15 +1,24 @@
 package util_test
 
 type MockExiter struct {
-	WasExited bool
+	WasExited    bool
+	ErrorMessage string
+}
+
+func NewMockExiter() *MockExiter {
+	return &MockExiter{
+		WasExited: false,
+	}
 }
 
 func (mockExiter *MockExiter) ExitWithError(err error) {
 	mockExiter.WasExited = true
+	mockExiter.ErrorMessage = err.Error()
 	return
 }
 
 func (mockExiter *MockExiter) ExitWithErrorMessage(errorMessage string) {
 	mockExiter.WasExited = true
+	mockExiter.ErrorMessage = errorMessage
 	return
 }
