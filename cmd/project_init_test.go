ac6906367471041fa1e47ec12eeb6ea9d6ef0429
Steven Thomas <stevent3115@gmail.com>
2020-06-15 14:03:05 -0400

    Added ProjectInitCmd test cases.

--- cmd/project_init_test.go
+++ cmd/project_init_test.go
@@ -47,6 +47,186 @@ func TestProjectInitCmd(t *testing.T) {
 					},
 				),
 		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from os.Create() is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockFileSystemDelegaterBuilder(
+					utiltest.NewMockFileSystemDelegaterBuilder().
+						SetUseDefaultCreateFileError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultCreateFileError).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": make([][]string, 0),
+					},
+				),
+		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from running 'git init' is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockGitManagerCreatorBuilder(
+					utiltest.NewMockGitManagerCreatorBuilder().
+						SetUseDefaultInitError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultInitError).
+				SetMockFileBuilders(
+					utiltest.NewMockFileBuilder().
+						SetFilePath(config.MppmConfigFileName).
+						SetContentsFromBytes(
+							configtest.GetDefaultMppmConfigAsJson(),
+						).
+						SetWasClosed(true),
+				).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": [][]string{
+							[]string{"init"},
+						},
+					},
+				),
+		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from running 'git lfs install' is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockGitManagerCreatorBuilder(
+					utiltest.NewMockGitManagerCreatorBuilder().
+						SetUseDefaultLfsInstallError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultLfsInstallError).
+				SetMockFileBuilders(
+					utiltest.NewMockFileBuilder().
+						SetFilePath(config.MppmConfigFileName).
+						SetContentsFromBytes(
+							configtest.GetDefaultMppmConfigAsJson(),
+						).
+						SetWasClosed(true),
+				).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": [][]string{
+							[]string{"init"},
+							[]string{"lfs", "install"},
+						},
+					},
+				),
+		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from running 'git lfs track' is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockGitManagerCreatorBuilder(
+					utiltest.NewMockGitManagerCreatorBuilder().
+						SetUseDefaultLfsTrackError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultLfsTrackError).
+				SetMockFileBuilders(
+					utiltest.NewMockFileBuilder().
+						SetFilePath(config.MppmConfigFileName).
+						SetContentsFromBytes(
+							configtest.GetDefaultMppmConfigAsJson(),
+						).
+						SetWasClosed(true),
+					utiltest.NewMockFileBuilder().
+						SetFilePath(".gitignore").
+						SetContentsFromString(
+							strings.Join(applications.GetAllFilePatternsConfig().GitIgnorePatterns, "\n"),
+						).
+						SetWasClosed(true),
+				).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": [][]string{
+							[]string{"init"},
+							[]string{"lfs", "install"},
+							append([]string{"lfs", "track"}, applications.GetAllFilePatternsConfig().GitLfsTrackPatterns...),
+						},
+					},
+				),
+		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from running 'git add' is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockGitManagerCreatorBuilder(
+					utiltest.NewMockGitManagerCreatorBuilder().
+						SetUseDefaultAddError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultAddError).
+				SetMockFileBuilders(
+					utiltest.NewMockFileBuilder().
+						SetFilePath(config.MppmConfigFileName).
+						SetContentsFromBytes(
+							configtest.GetDefaultMppmConfigAsJson(),
+						).
+						SetWasClosed(true),
+					utiltest.NewMockFileBuilder().
+						SetFilePath(".gitignore").
+						SetContentsFromString(
+							strings.Join(applications.GetAllFilePatternsConfig().GitIgnorePatterns, "\n"),
+						).
+						SetWasClosed(true),
+				).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": [][]string{
+							[]string{"init"},
+							[]string{"lfs", "install"},
+							append([]string{"lfs", "track"}, applications.GetAllFilePatternsConfig().GitLfsTrackPatterns...),
+							[]string{"add", ".gitignore", ".gitattributes", config.MppmConfigFileName},
+						},
+					},
+				),
+		},
+
+		&ProjectInitCmdTestCase{
+			description: "Test that any error from running 'git commit' is properly raised.",
+			mockExecutionEnvironmentBuilder: utiltest.NewMockExecutionEnvironmentBuilder().
+				SetMockGitManagerCreatorBuilder(
+					utiltest.NewMockGitManagerCreatorBuilder().
+						SetUseDefaultCommitError(true),
+				),
+			expectedExecutionEnvironmentStateBuilder: utiltest.NewMockExecutionEnvironmentStateBuilder().
+				SetExiterWasExited(true).
+				SetExiterError(utiltest.DefaultCommitError).
+				SetMockFileBuilders(
+					utiltest.NewMockFileBuilder().
+						SetFilePath(config.MppmConfigFileName).
+						SetContentsFromBytes(
+							configtest.GetDefaultMppmConfigAsJson(),
+						).
+						SetWasClosed(true),
+					utiltest.NewMockFileBuilder().
+						SetFilePath(".gitignore").
+						SetContentsFromString(
+							strings.Join(applications.GetAllFilePatternsConfig().GitIgnorePatterns, "\n"),
+						).
+						SetWasClosed(true),
+				).
+				SetGitManagerInputHistoriesIndexedByRepoPath(
+					map[string][][]string{
+						".": [][]string{
+							[]string{"init"},
+							[]string{"lfs", "install"},
+							append([]string{"lfs", "track"}, applications.GetAllFilePatternsConfig().GitLfsTrackPatterns...),
+							[]string{"add", ".gitignore", ".gitattributes", config.MppmConfigFileName},
+							[]string{"commit", "-m", "Initial commit."},
+						},
+					},
+				),
+		},
 	}
 
 	for _, testCase := range testCases {
